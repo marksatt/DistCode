7cd817104fc106b3fd2e2991f46f1ce02e1d9f94
Mark Satterthwaite <marksatt@users.noreply.github.com>
2013-10-18 09:45:01 +0100

    [distclang] Don’t permit recursion and execute gethost & xcrun to ensure we call the correct compiler - distclang relies upon masquerading.

--- distclang/main.cpp
+++ distclang/main.cpp
@@ -7,13 +7,50 @@
 //
 
 #include <iostream>
+#include <string>
+#include <unistd.h>
 
 int main(int argc, const char * argv[])
 {
+	char** newArgv = (char**)alloca(argc+2*sizeof(char const*));
+
+	char const* path = argv[0];
+	size_t pathLen = strlen(path);
+	for (size_t i = pathLen; i; --i)
+	{
+		if(path[i-1]=='/')
+		{
+			path = &path[i];
+			break;
+		}
+	}
+	
+	char *env = getenv("DISTCLANG_RECURSE");
+	if(env != NULL)
+	{
+		fprintf(stderr, "distclang called recursively!\n");
+		return -1;
+	}
+	putenv("DISTCLANG_RECURSE=1");
+	
+	const char *envpath;
+    if (!(envpath = getenv("PATH")))
+		/* strange but true*/
+        return 0;
+	
+	std::string getHostPath(".");
+	getHostPath += "/gethost";
+	newArgv[0] = (char*)"xcrun";
+	newArgv[1] = (char*)path;
+	
+	for (int i = 1; i < argc; i++) {
+		newArgv[i+1] = (char*)argv[i];
+	}
+	
 	int forkret = fork();
     if (forkret == 0) {
 		/* child process */
-		if (execvp(argv[nextarg], &argv[nextarg]) < 0) {
+		if (execvp(getHostPath.c_str(), newArgv) < 0) {
 			fprintf(stderr, "execvp failed: err %s\n", strerror(errno));
 			return -1;
 		}
