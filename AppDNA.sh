6232745dbcc81b970ef402b7f412ef49db482a62
Mark Satterthwaite <marksatt@users.noreply.github.com>
2014-05-14 23:14:14 +0100

    [DistCode] Sparkle updates hooked up.

--- AppDNA.sh
+++ AppDNA.sh
@@ -15,7 +15,7 @@ rm -rf "$INSIDE_SOURCE"
 mkdir -p "$INSIDE_SOURCE"
 
 # Copy the source tree, minus resources, into Content/Source.
-rsync -a --cvs-exclude --extended-attributes \
+rsync -a --cvs-exclude --extended-attributes --exclude 'NoRedist' --exclude 'Updates' \
 --exclude 'build' --exclude '*.pbxuser' --exclude '*.mode1' --exclude '.git' \
 --exclude '*.scriptSuite' --exclude '*.o' --exclude '*.d' \
 --exclude '*.iconset' --exclude 'xcuserdata' --exclude '*.mode1v3' --exclude '*.perspectivev3' \
