86b6d04c49e36843d8cc1f81e4c8b8dbf25cfcce
Mark Satterthwaite <marksatt@users.noreply.github.com>
2015-03-21 16:58:04 +0000

    [lzo] Mac build projects.

---
+++ lzo-2.08/MacBuild.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+olddir=`pwd`
+cd $srcdir
+
+echo "Working Dir: $srcdir"
+
+$srcdir/configure --enable-shared --prefix=$srcdir/bin || {
+	echo "error: configure failed"
+	exit 1
+}
+
+make || {
+	echo "error: make failed"
+	exit 1
+}
+
+make install || {
+	echo "error: make install failed"
+	exit 1
+}
