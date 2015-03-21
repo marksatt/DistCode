308382f565cd9d29a63b9d330b4f6007afee4047
Mark Satterthwaite <marksatt@users.noreply.github.com>
2015-03-21 17:08:01 +0000

    [dsticc][dmucs] Remove distcc & DMUCS.

--- distcc/src/cpp_dialect.h
+++
@@ -1,6 +0,0 @@
-
-extern int dcc_seen_opt_x;
-extern char const*dcc_opt_x_ext;
-
-char const*dcc_ext_lookup(char const*key);
-void dcc_fix_opt_x(char **argv);
--- dmucs/dmucs/COSMIC/Makeskt.bat
+++
@@ -1,9 +0,0 @@
-cl -c *.c /AS 
-lib @makelib
-cd  exe
-cl -c /AS -I\c600\socket *.c
-cd ..
-link exe\sktdbg ,,,smplskts+socket+slibce+winapp,/noi /noe
-link exe\spmtable,,,smplskts+socket+slibce+winapp,/noi /noe
-link exe\srmsrvr ,,,smplskts+socket+slibce+winapp,/noi /noe
-mv *.exe c:\c600\socket\exe
